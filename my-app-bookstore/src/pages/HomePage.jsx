14a7397ddb0c9d4fa219e3cfe5848c005ef21f89
Thanaphat <sukchuen_t@su.ac.th>
2025-09-19 15:48:49 +0000

    bookstore101

---
+++ my-app-bookstore/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css'; // ✨ Import from style folder
+// import SearchBar from '../components/SearchBar';
+// import BookCard from '../components/BookCard';
+// import { getFeaturedBooks, getCategories } from '../data/booksData';
+
+const HomePage = () => {
+
+  return (
+    <div className="home-page">
+      {/* Hero Section */}
+      Hello
+    </div>
+  );
+};
+
+export default HomePage;
